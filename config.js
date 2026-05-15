b77e3a3adc24ec3a9d97e5fac9032e17a912b04c
Jan Tschada <j.tschada@esri.de>
2026-05-15 09:23:56 +0200

    Updated config workflow

--- config.js
+++ config.js
@@ -1,8 +1,4 @@
 esriConfig = {
     // Set the API key to access basemaps and other location services
     apiKey: "a1dKxlny41Do6BuroZ59_jA..fMLJwViE3HAPCOhAF7KCjLb4zYIULa25RfKbS9v3OAsUNIJVS5ZE7_sMLn01UdqglPFO0N3-_SQAOQNeVUqXM__8g3fhz1UgudCsgnwS1DHxfyGhfEc-M4Ggg0G9PcUILE7aG0e4BsUdx14.",
-    env: {
-        client_id: "Jk3yPBf8EU5x0VPi",
-        client_secret: "3923e666996042358fa4761f99dec270"
-    }
 }
